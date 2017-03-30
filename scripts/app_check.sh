133e30330c42a06846357ee8757e6e39ffa23388
Patrick F. Marques <patrick.marques@automationlogic.com>
2017-03-30 20:32:32 +0100

    Fix app check script

--- scripts/app_check.sh
+++ scripts/app_check.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-set -xe
+set -xu
 
 PORT=3000
 
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "localhost:${PORT}")
+for i in {1..10} ; do
+	STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "localhost:${PORT}")
 
-if [ ${STATUS_CODE} -eq 200 ]; then
-    echo "OK Application is heathy!"
-    exit 0
-else
-    echo "ERROR: Application is not heathy!"
-    exit 1
-fi
+	if [ ${STATUS_CODE} -eq 200 ]; then
+		echo "OK Application is heathy!"
+		exit 0
+	fi
+
+	sleep 1
+done
+
+echo "ERROR: Application is not heathy!"
+exit 1
