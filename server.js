36b7b6ab90fa720bcdf7d6579a238af93d5fbd20
Xavier Thomas <xavier.thomas@automationlogic.com>
2017-04-24 09:44:54 +0100

    Changed Morgan logging format.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var methodOverride = require('method-override');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
-app.use(morgan('dev'));
+app.use(morgan(':remote-addr [:date[clf]] :method :url :status :res[content-length] - :response-time ms ":user-agent"'));
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
