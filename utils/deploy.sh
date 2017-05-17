a8b6bf036b19cf40212ae12005bc2494b560d76f
Kamil Forys <kamil.forys@gmail.com>
2017-05-17 14:04:35 +0100

    Resolving shellcheck failures in deploy.sh

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -92,12 +92,12 @@ aws cloudformation describe-stacks \
 
 # Deploy Kibana dashboards.
 (
-PWD=$(basename `pwd`)
-if [ $PWD == 'aws-cloud-platform' ]; then
+PWD=$(basename "$(pwd)")
+if [ "$PWD" == 'aws-cloud-platform' ]; then
   cd kibana;
-elif [ $PWD == 'utils' ]; then
+elif [ "$PWD" == 'utils' ]; then
   cd ../kibana
 fi
 ES_URL=$(aws --region "${REGION}" cloudformation describe-stacks --stack-name "${STACK_NAME}" --query "Stacks[0].Outputs[?OutputKey=='KibanaURL'].OutputValue" --output text | cut -f 1-3 -d'/')
-./load.sh -url $ES_URL
+./load.sh -url "$ES_URL"
 )
