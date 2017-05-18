0c5297a92ab1d1d91abe240f06db68033fa8b019
Patrick Marques <patrick.marques@automationlogic.com>
2017-05-18 11:17:57 +0000

    Fix cleanup all parameters
    Approved-by: Xavier Thomas <xavier.thomas@automationlogic.com>

--- utils/cleanup/del_all_params.sh
+++ utils/cleanup/del_all_params.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-set -xu
+set -u
 
 REGION="${REGION:-eu-west-1}"
 
-PARAMS=$(aws --region "${REGION}" ssm describe-parameters --query 'Parameters[*].{Name:Name}' --output text)
-for param in $PARAMS ; do
-	aws --region "${REGION}" ssm delete-parameter --name "${param}"
+NEXT=""
+NEXT_T=""
+while [ "${NEXT}" != "null" ]; do
+	RESULTS=$(aws --region "${REGION}" ssm describe-parameters --output json --max-results 50 ${NEXT_T})
+	NEXT=$(echo "${RESULTS}" | jq -r '.NextToken')
+	PARAMS=$(echo "${RESULTS}" | jq -r '.Parameters[].Name')
+	NEXT_T="--next-token ${NEXT}"
+
+	for param in ${PARAMS}; do
+		aws --region "${REGION}" ssm delete-parameter --name "${param}"
+	done
 done
