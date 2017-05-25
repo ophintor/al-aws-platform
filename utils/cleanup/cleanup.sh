e8da4ef65dfbe4acaeecb65aa87d20045f15545a
Xavier Thomas <xavier.thomas@automationlogic.com>
2017-05-25 12:57:20 +0000

    Updated cleanup script
    Approved-by: Patrick Marques <patrick.marques@automationlogic.com>

--- utils/cleanup/cleanup.sh
+++ utils/cleanup/cleanup.sh
@@ -44,8 +44,22 @@ done
 declare -a LOG_GROUPS=(
 	"/aws/codebuild/${STACK_NAME}-myapp"
 	"/aws/codebuild/${STACK_NAME}-myapp-image"
+	"/aws/codebuild/${STACK_NAME}-application"
+	"/aws/codebuild/${STACK_NAME}-kibana"
 	"/aws/lambda/${STACK_NAME}-set-param-store"
+	"/aws/lambda/${STACK_NAME}-LogMigrationLambda"
+	"/aws/lambda/${STACK_NAME}-LogStreamer"
+	"/aws/lambda/${STACK_NAME}-ParameterStoreLambda"
+	"/aws/lambda/${STACK_NAME}-SnapshotCreateLambda"
+	"/aws/lambda/${STACK_NAME}-SnapshotRepoCreateLambda"
+	"/aws/lambda/${STACK_NAME}-ELBLogsToCWLambda"
 	"${STACK_NAME}-ecs"
+	"${STACK_NAME}-apperr"
+	"${STACK_NAME}-applog"
+	"${STACK_NAME}-cloudinitoutput"
+	"${STACK_NAME}-cloudtrail"
+	"${STACK_NAME}-elblog"
+	"${STACK_NAME}-syslog"  
 )
 
 LOG_GROUPS+=(
@@ -60,6 +74,10 @@ done
 # Cleanup CloudTrail S3 objects
 declare -a S3_BUCKETS=(
 	"${STACK_NAME}-codepipeline-artifacts"
+	"${STACK_NAME}-artifacts"
+	"${STACK_NAME}-elb-logs"
+	"${STACK_NAME}-logs"
+	"${STACK_NAME}-snapshots"
 )
 
 S3_BUCKETS+=(
