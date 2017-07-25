8f2c56952278a0c398eb054f0ba316fc01f1cc79
Sebastian Norris <sebastian@automationlogic.com>
2017-07-25 15:21:19 +0000

    Log tamper alarm
    * Added LogTamperMetricFilter, which filters for unauthorised operations on S3 buckets.
    
    * Added LogTamperAlarm, an alarm that e-mails the stack creator if there is an unauthorised operation on an S3 bucket.
    
    * Added TamperTrail, a CloudTrail trail that logs write operations on S3 objects across S3 log buckets (main logs, not ELB logs) and stores them in the current stack’s log bucket under a directory called WriteOps.
    
    * Added TamperLambdaTrigger, a custom resource that triggers TamperLambda
    
    * Added TamperLambda, which configures the TamperTrail data event settings to track all log buckets.
    
    * Added TamperLambdaRole, which allows the lambda to use cloudtrail and S3 actions.
    
    Approved-by: Matt Peperell <matt@peperell.com>

---
+++ lambda/TamperLambda.py
@@ -0,0 +1,62 @@
+import boto3
+import base64
+import uuid
+import httplib
+import urlparse
+import json
+import cfnresponse
+
+def send_response(request, response, context, status=None, reason=None):
+  if status is not None:
+    response['Status'] = status
+  if reason is not None:
+    response['Reason'] = reason
+  try:
+    if 'ResponseURL' in request and request['ResponseURL']:
+      url = urlparse.urlparse(request['ResponseURL'])
+      body = json.dumps(response)
+      https = httplib.HTTPSConnection(url.hostname)
+      https.request('PUT', url.path+'?'+url.query, body)
+      cfnresponse.send(request, context, cfnresponse.SUCCESS, response, response['PhysicalResourceId'])
+  except Exception as e:
+    cfnresponse.send(request, context, cfnresponse.FAILED, response, response['PhysicalResourceId'])
+  return response
+
+def handler(event, context):
+  client = boto3.client('cloudtrail')
+  s3client = boto3.client('s3')
+
+  body = s3client.list_buckets()
+  bucketList = []
+  buckets_remain = True
+  bucket_num = 0
+  while( buckets_remain == True ):
+    try:
+      bucketList.append(body["Buckets"][bucket_num]["Name"])
+      bucket_num += 1
+    except:
+      buckets_remain = False
+  bucketList = ["arn:aws:s3:::"+x+"/AWS" for x in bucketList if "logs" in x and "elb" not in x]
+
+  response = client.put_event_selectors(
+    TrailName = event['ResourceProperties']['TamperTrail'],
+    EventSelectors=[
+      {
+        'ReadWriteType': 'WriteOnly',
+        'IncludeManagementEvents': False,
+        'DataResources': [
+          {
+            'Type': 'AWS::S3::Object',
+            'Values': bucketList
+          },
+        ]
+      },
+    ]
+  )
+  if 'PhysicalResourceId' in event:
+    response['PhysicalResourceId'] = event['PhysicalResourceId']
+  else:
+    response['PhysicalResourceId'] = str(uuid.uuid4())
+  if event['RequestType'] == 'Delete':
+    return send_response(event, response, context)
+  return send_response(event, response, context)
